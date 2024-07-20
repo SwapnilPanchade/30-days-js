8623633515da024663ce3a554f823d7f807ab75d
SwapnilPanchade <sspanchade113@gmail.com>
2024-07-20 15:30:43 +0530

    Just completed the first assignment today

---
+++ day-01/Readme.md
@@ -0,0 +1,3 @@
+## This is the first assingment this is actually very basic
+
+    - well you will decleare variables in here and assign values to them
---
+++ day-01/day1.js
@@ -0,0 +1,10 @@
+//? Declearing using var, let
+var a = 1;
+let b = 2;
+
+// using const opearator
+const num = 20;
+let arr = [20, 5, 68];
+//
+//prettier-ignore
+console.log(typeof (arr));
